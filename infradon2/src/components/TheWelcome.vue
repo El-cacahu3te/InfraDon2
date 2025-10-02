c820ad0bbd69a4dc706e871faeefa2aa00f7ec9a
El-cacahu3te <elia.nicolo@outlook.com>
2025-10-02 15:43:15 +0200

    init Vue

---
+++ infradon2/src/components/TheWelcome.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+// DATA - MODEL
+const counter = ref(10)
+
+// METHODS - CONTROLLER
+const increment = () => {
+  counter.value++
+}
+const reset = () => {
+  counter.value = 0
+}
+</script>
+
+<template>
+  <h1>Hello World</h1>
+  <button @click="increment">Increment</button>
+  <button @click="reset">Reset</button>
+  <p style="color: black; font-size: 50px">{{ counter }}</p>
+</template>
